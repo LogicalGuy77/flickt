b79b290c0c25e971d7b1900704a81e0dfde4e5f3
LogicalGuy77 <harshitacademia@gmail.com>
2024-10-27 23:50:03 +0530

    fixed linting

--- next.config.js
+++ next.config.js
@@ -7,6 +7,22 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  images: {
+    domains: [
+      "assets.aceternity.com",
+      "plus.unsplash.com",
+      "images.unsplash.com",
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["assets.aceternity.com" , 'plus.unsplash.com' , 'images.unsplash.com'], // Allow external images from this domain
-  },
-};
-
-export default nextConfig;
